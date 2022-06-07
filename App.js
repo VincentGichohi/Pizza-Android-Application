cb54f35c6542e08dd6e6d615ba6750d3f22e61df
VincentGichohi <vincentegichohi@gmail.com>
2022-06-07 15:46:20 +0300

    Add a react app and its dependencies

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const mytext = "by ProgramWithUs"
+    return ( <View style = { styles.container } >
+        <StatusBar style = "auto" />
+        <Image
+        style={styles.pizzaImage}
+        source={{
+        uri: "https://cdn.pixabay.com/photo/2017/12/05/20/10/pizza-3000285_960_720.png",
+        }}
+        />
+        <Text style = { styles.baseText } > Pizza vs.Pizza App </Text>
+        <Text style = {styles.newText}>{mytext}</Text>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    baseText: {
+        color: "navy",
+        fontSize: 30,
+        fontStyle: "italic",
+    },
+    newText: {
+        color: "red",
+    },
+    pizzaImage: {
+        width: 200,
+        height: 200,
+    },
 });
